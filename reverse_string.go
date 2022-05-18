e5aa79223fa6c6497027ed12fecefc197fa5e3e9
skoflok <skoflok@gmail.com>
2022-05-18 19:01:50 +0400

    implement reverse func

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	r := []rune(input)
+	ro := []rune{}
+	for i := len(r) - 1; i >= 0; i-- {
+		ro = append(ro, r[i])
+	}
+	output = string(ro)
 	return output
 }
